730a8acb0559b853c2a9eb733426506f904363db
Rolando Martínez <A01177704@itesm.mx>
2021-08-27 09:40:42 -0500

    primer entrega de la tarea

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -2,6 +2,18 @@
 def main():
     edad = int(input("Ingresa tu edad: "))
     #Aquí empieza tu programa...
+    if (edad >= 18):
+        identificacionOficial = input("¿Tienes identificación oficial? (s/n): ")
+        if (identificacionOficial == 'S' or identificacionOficial == 's'):
+            print("Trámite de licencia concedido")
+        elif (identificacionOficial == 'N' or identificacionOficial == 'n'):
+            print("No cumples requisitos")
+        else:
+            print("Respuesta incorrecta")
+    elif (edad < 18 and edad > 0):
+        print("No cumples requisitos")
+    else:
+        print("Respuesta incorrecta")
 
 if __name__ == '__main__':
     main()
--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -4,10 +4,12 @@ def main():
     num2 = int(input("Ingresa el segundo número: "))
     num3 = int(input("Ingresa el tercer número: "))
 
-    if ???:
+    if ((num1 > num2 and num1 > num3) or (num1 == num2 and num1 > num3) or (num1 == num3 and num1 > num2)):
         print(num1)
-    elif ???: 
+    elif ((num2 > num1 and num2 > num3) or (num2 == num1 and num2 > num3) or (num2 == num3 and num2 > num1)):
         print(num2)
+    elif ((num3 > num1 and num3 > num2) or (num3 == num1 and num3 > num2) or (num3 == num2 and num3 > num1)):
+        print(num3)
     else:
         print(num3)
 
